b7ad0f910e798a6a5c81d007dc9a24323d4ddf7c
Lars <45746485+Freshairkaboom@users.noreply.github.com>
2021-06-09 14:13:05 +0200

    Update SSPmodel.js

--- SSPmodel.js
+++ SSPmodel.js
@@ -1,1 +1,8 @@
-// model
+//Model
+let appen = getId("app");
+let html = "";
+let showResult = getId('showResult');
+let valgt = "";
+let robotValg = 2;
+let resultat = "Velg stein, saks eller papir";
+let robotBilde = "blank.png";
