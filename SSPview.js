cabd47f376914f62d107a4f7e8a049df8c9f08b1
Lars <45746485+Freshairkaboom@users.noreply.github.com>
2021-06-09 14:12:45 +0200

    Update SSPview.js

--- SSPview.js
+++ SSPview.js
@@ -1,1 +1,21 @@
-// view
+//View
+sspView()
+function sspView(){
+    html = `
+    <h1>Stein, saks, papir!</h1>
+    <hr>
+    <div id="showResult"><h1>${resultat}</h1></div>
+    <div class="board">
+    <img onclick="valgtNoe(1)" src="rock.png" alt="Stein">
+    <img onclick="valgtNoe(2)" src="scissor.png" alt="Saks">
+    <img onclick="valgtNoe(3)" src="paper.png" alt="Paper">
+    </div>
+    </br>
+    <div class="robotboard">
+    <div id="scoreboard"></div>
+    <div><img id="robotSlave" src="Robot.png" alt="Robotmann"></div>
+    <img class="robotValg" src="${robotBilde}" alt="Robotvalg">
+    </div>
+    `;
+    appen.innerHTML = html;
+}
