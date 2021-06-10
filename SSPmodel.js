5916728eeaaae3b459db3309e093aedf8e38236e
qqgameover <kasper35@live.no>
2021-06-10 13:16:37 +0200

    Update SSPmodel.js

--- SSPmodel.js
+++ SSPmodel.js
@@ -1,5 +1,6 @@
 //Model
 let appen = getId("app");
+let viewValgt = "index";
 let html = "";
 let showResult = getId('showResult');
 let valgt = "";
@@ -8,3 +9,23 @@ let resultat = "Velg stein, saks eller papir";
 let robotBilde = "blank.png";
 let youScore = 0;
 let robotScore = 0;
+let status = "";
+let restarted = ""
+let blackJack = {
+	playerValue: 0,
+	blackJackBust: false,
+	blackJackbj: false,
+	blackJackAiBust: false,
+	currentCards: [],
+	currentAiCards: [],
+	aiValue: 0,
+	players: 1,
+	winner: 0,
+	kortahtml: "",
+	aiKorta: ""
+};
+
+
+function getId(id) {
+	return document.getElementById(id);
+}
