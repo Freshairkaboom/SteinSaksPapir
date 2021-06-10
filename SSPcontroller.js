4aeafda9b8a6582079d0c1d5c54a97c8126d5d5a
qqgameover <kasper35@live.no>
2021-06-10 13:16:35 +0200

    Update SSPcontroller.js

--- SSPcontroller.js
+++ SSPcontroller.js
@@ -3,7 +3,7 @@ function valgtNoe(type) {
     if (type === 1) {
         valgt = "Stein";
     }
-    else if (type === 2){
+    else if (type === 2) {
         valgt = "Saks"
     } else {
         valgt = "Papir";
@@ -26,11 +26,11 @@ function vinner() {
         resultat = "Stein slår saks, du vant!";
         youScore++;
     }
-    else if(valgt == "Saks" && robotValg == 3) {
+    else if (valgt == "Saks" && robotValg == 3) {
         resultat = "Saks slår papir, du vant!";
         youScore++;
     }
-    else if(valgt == "Papir" && robotValg == 1) {
+    else if (valgt == "Papir" && robotValg == 1) {
         resultat = "Papir slår stein, du vant!"
         youScore++;
     }
@@ -38,28 +38,22 @@ function vinner() {
         resultat = "Papir slår stein, du tapte!";
         robotScore++;
     }
-    else if(valgt == "Saks" && robotValg == 1) {
+    else if (valgt == "Saks" && robotValg == 1) {
         resultat = "Stein slår saks, du tapte!";
         robotScore++;
     }
-    else if(valgt == "Papir" && robotValg == 2) {
+    else if (valgt == "Papir" && robotValg == 2) {
         resultat = "Saks slår papir, du tapte!"
         robotScore++;
     }
     else if (valgt == "Stein" && robotValg == 1) {
         resultat = "Begge valgte stein, uavgjort!";
     }
-    else if(valgt == "Saks" && robotValg == 2) {
+    else if (valgt == "Saks" && robotValg == 2) {
         resultat = "Begge valgte saks, uavgjort!";
     }
-    else if(valgt == "Papir" && robotValg == 3) {
+    else if (valgt == "Papir" && robotValg == 3) {
         resultat = "Begge valgte papir, uavgjort!"
     }
     sspView();
-}
-
-
-//Rene funksjoner
-function getId(id) {
-    return document.getElementById(id);
 }
